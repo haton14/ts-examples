6f880dcec167790ad6eee4e88d495909a8d43353
haton14 <iioka.takumi.quena@gmail.com>
2023-01-22 17:42:02 +0900

    call back

--- fetch/src/index.ts
+++ fetch/src/index.ts
@@ -1,3 +1,8 @@
-const message: string = 'Hello';
-
-console.log(message);
+type User = { name: string; age: number };
+const userList: User[] = [
+    { name: 'unko', age: 1 },
+    { name: 'unkounko', age: 18 }
+];
+console.log(userList);
+const nameList = userList.map((u: User): string => u.name);
+console.log(nameList);
