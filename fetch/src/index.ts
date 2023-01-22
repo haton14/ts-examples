74851de737404a32353c6c57fa581223621cfb1e
haton14 <iioka.takumi.quena@gmail.com>
2023-01-22 22:22:24 +0900

    fetch

--- fetch/src/index.ts
+++ fetch/src/index.ts
@@ -5,3 +5,12 @@ const p = readFile('unko.txt', 'utf-8');
 p.then((data) => {
     console.log(data);
 });
+
+const res = fetch('https://nodejs.org/api/documentation.json')
+res
+    .then((data) => {
+        return data.json()
+    })
+    .then((body) => {
+        console.log(body)
+    })
