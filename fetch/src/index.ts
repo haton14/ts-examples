d4b0c0bc8001cf778eea95758435b39b3196a470
haton14 <iioka.takumi.quena@gmail.com>
2023-01-29 23:27:52 +0900

    fetchを型付けでラップ

--- fetch/src/index.ts
+++ fetch/src/index.ts
@@ -2,15 +2,40 @@ import { readFile } from "node:fs/promises";
 
 const p = readFile('unko.txt', 'utf-8');
 
+type Dinner = {
+    id: number;
+    createdAt: number;
+    updatedAt: number;
+}
+
+const wrap = <T>(task: Promise<Response>): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        task.then((res) => {
+            if (res.ok) {
+                res.json().then((body) => {
+                    resolve(body)
+                }).catch((error) => {
+                    reject(error)
+                })
+            } else {
+                reject(res)
+            }
+        }).catch((error) => {
+            reject(error)
+        })
+    })
+}
+const fetcher = <T = any>(
+    input: RequestInfo,
+    init?: RequestInit
+): Promise<T> => {
+    return wrap<T>(fetch(input, init))
+}
+
 p.then((data) => {
     console.log(data);
 });
-
-const res = fetch('https://nodejs.org/api/documentation.json')
-res
-    .then((data) => {
-        return data.json()
-    })
-    .then((body) => {
-        console.log(body)
-    })
+const res = fetcher<Dinner>('https://api.private.ohagi.link/dinners/1')
+res.then((data) => {
+    console.log(data)
+})
